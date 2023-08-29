6eaf1f549223d6d5d62142bd6360c1989d483983
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-08-29 21:21:20 +0900

    feature: Essentials - Template Syntax (Raw HTML まで)

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -3,7 +3,9 @@ import { createApp } from 'vue'
 const app = createApp({
   data() {
     return {
-      count: 0
+      count: 0,
+      msg: 'hello',
+      rawHtml: `<span style="color: red">This should be red.</span>`
     }
   }
 })
